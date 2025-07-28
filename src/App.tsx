80c6809aa77ac7ea4277316cbfa22ab2d99c8990
Sk Rajibul Islam <rajibul2001@gmail.com>
2025-07-29 00:42:02 +0530

    Rename layouts to layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import AppLayout from "@/components/layouts/AppLayout";
+import AppLayout from "@/components/layout/AppLayout";
 
 import DashboardPage from "./pages/DashboardPage";
 import NotFound from "./pages/NotFound";
